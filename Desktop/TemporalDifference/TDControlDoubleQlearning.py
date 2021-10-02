03d7ad2be77253968a0d80b5f80d3ccba7fed693
kimh060612 <kimh060612@naver.com>
2021-10-02 23:05:54 +0900

    .

--- Desktop/TemporalDifference/TDControlDoubleQlearning.py
+++ Desktop/TemporalDifference/TDControlDoubleQlearning.py
@@ -1,5 +1,6 @@
 import numpy as np
 import gym
+import matplotlib.pyplot as plt
 
 GAMMA=0.9
 EPISODES=100000
@@ -21,36 +22,39 @@ class TDAgent:
         if np.random.rand() < self.EPS:
             return np.random.randint(0, 4)
         else : 
-            return np.argmax(self.Q1Function[state] + self.Q2Function[state])
+            return np.argmax(self.Q1Function[state, :] + self.Q2Function[state, :])
 
     def update(self, state, action, reward, state_next):
         if np.random.rand() < 0.5 :
             a_ = np.argmax(self.Q1Function[state_next])
-            self.Q1Function[state, action] += (reward + self.Discount * self.Q1Function[state_next, a_] - self.Q1Function[state, action])
+            self.Q1Function[state, action] += (reward + self.Discount * self.Q2Function[state_next, a_] - self.Q1Function[state, action])
         else :
             a_ = np.argmax(self.Q2Function[state_next])
-            self.Q2Function[state, action] += (reward + self.Discount * self.Q2Function[state_next, a_] - self.Q2Function[state, action])
+            self.Q2Function[state, action] += (reward + self.Discount * self.Q1Function[state_next, a_] - self.Q2Function[state, action])
 
 if __name__ == "__main__":
     env = gym.make('FrozenLake-v1')
     agent = TDAgent(16, 4)
-    total_reward_train = 0
+    train_reward_list = []
 
     for epoch in range(EPISODES):
         observation = env.reset()
         state_ = 0
-        
-        for t in range(1000):
+        total_reward_train = 0
+        t = 0
+        while True:
             env.render()
             action = agent.get_action(state_)
             observation, reward, done, info = env.step(action)
             agent.update(state=state_, action=action, reward=reward, state_next=observation)
             state_ = observation
+            total_reward_train += reward
+            t += 1
             if done:
                 print("Episode finished after {} timesteps".format(t+1))
-                total_reward_train += reward
                 break
-
+        train_reward_list.append(total_reward_train)
+    
     total_reward_test = 0
     for i_episode in range(TEST_EPISODE):
         observation = env.reset()
@@ -67,8 +71,10 @@ if __name__ == "__main__":
                 print("Episode finished after {} timesteps".format(t+1))
                 break
     
-    print(total_reward_train / EPISODES)
+    print(sum(train_reward_list) / EPISODES)
     print(total_reward_test / TEST_EPISODE)
     env.close() 
     
     print(np.argmax(agent.Q1Function + agent.Q2Function, axis=1).reshape(4, 4))
+    plt.plot(train_reward_list)
+    plt.show()
